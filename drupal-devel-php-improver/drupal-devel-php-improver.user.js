c00a548983675ea57e13eb628b71996364fbd887
John <j.sanderson@ltl.uk.com>
2014-06-05 11:57:48 +0100

    added support for display suite code editor

--- drupal-devel-php-improver/drupal-devel-php-improver.user.js
+++ drupal-devel-php-improver/drupal-devel-php-improver.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
-// @name          Drupal devel/php Improver
+// @name          Drupal PHP Input Improver
 // @author        John Sanderson <@9point6>
-// @description   Adds CodeMirror to the devel/php page
-// @namespace     http://www.9point6.com/userscript/drupal-devel-php-improver
+// @description   Adds CodeMirror to certain fields on a Drupal site
+// @namespace     http://www.9point6.com/userscript/drupal-php-input-improver
 // @include       */devel/php
-// @version       1.0.1
+// @include       */admin/structure/ds/fields/manage_custom/*"
+// @version       1.1.0
 // ==/UserScript==
 
 (function ( ) {
-	var base, fn;
+	var base, fn, loadCodeMirror;
 
 	// Choose the version of codemirror to load
 	base = '//cdnjs.cloudflare.com/ajax/libs/codemirror/3.21.0/';
@@ -39,6 +40,82 @@
 		}
 
 		document.head.appendChild( e );
+	};
+
+	loadCodeMirror = function () {
+		var elem, selector, selection, page;
+
+		// Determine element
+		if (document.location.pathname.indexOf('/devel/php') !== -1) {
+			page = 'devel';
+			elem = document.getElementById('edit-code');
+
+		} else if (document.location.pathname.indexOf('/admin/structure/ds/fields/manage_custom') !== -1) {
+			page = 'display-suite';
+			selector = jQuery('.text-format-wrapper #edit-code-format--2');
+			selection = selector.val();
+
+			if(!window.initial) {
+				selector.click(function (e) {
+					selector = jQuery('.text-format-wrapper #edit-code-format--2');
+					selection = selector.val();
+					if (selection === 'php' || selection === 'ds_code') {
+						jQuery('.CodeMirror').show();
+					} else {
+						jQuery('.CodeMirror').hide();
+					}
+				});
+				window.initial = true;
+			}
+
+			if (selection === 'php' || selection === 'ds_code') {
+				elem = jQuery('.text-format-wrapper textarea')[0];
+			} else {
+				jQuery('.CodeMirror').remove();
+				return;
+			}
+		} else {
+			return;
+		}
+
+		// Customise styles
+		e = document.createElement('style');
+		e.appendChild( document.createTextNode(''));
+		document.head.appendChild(e);
+		e.sheet.addRule( '.CodeMirror', [
+			'height: 600px;',
+			'font-size: 1.4em;',
+			'font-family: "Inconsolata", "Source Code Pro", "Consolas", monospace;',
+		].join(' '));
+		e.sheet.addRule('.CodeMirror-gutters', [
+			'background: #36372F !important;'
+		].join(' '));
+
+		// Create CodeMirror on the textarea
+		CodeMirror.fromTextArea(elem, {
+			'lineNumbers': true,
+			'mode': 'application/x-httpd-php-open',
+			'matchBrackets': true,
+			'autoCloseBrackets': true,
+			'indentUnit': 4,
+			'indentWithTabs': true,
+			'theme': 'monokai',
+			'autofocus': true,
+			'extraKeys': {
+				'Ctrl-Enter': function (cm) {
+					switch (page) {
+						case 'devel':
+							jQuery('form[action="/devel/php"]').submit();
+							break;
+
+						case 'display-suite':
+							jQuery('form#ds-edit-custom-field-form').submit();
+							break;
+
+					}
+				}
+			}
+		});
 	};
 
 	// Let's get the party started
@@ -89,37 +166,7 @@
 		},
 		{
 			'elem': 'script',
-			'fn': function ( ) {
-				// Customise styles
-				e = document.createElement( 'style' );
-				e.appendChild( document.createTextNode( '' ) );
-				document.head.appendChild( e );
-				e.sheet.addRule( '.CodeMirror', [
-					'height: 600px;',
-					'font-size: 1.4em;',
-					'font-family: "Inconsolata", "Source Code Pro", "Consolas", monospace;',
-				].join( ' ' ) );
-				e.sheet.addRule( '.CodeMirror-gutters', [
-					'background: #36372F !important;'
-				].join( ' ' ) );
-
-				// Create CodeMirror on the textarea
-				CodeMirror.fromTextArea( document.getElementById( 'edit-code' ), {
-					'lineNumbers': true,
-					'mode': 'application/x-httpd-php-open',
-					'matchBrackets': true,
-					'autoCloseBrackets': true,
-					'indentUnit': 4,
-					'indentWithTabs': true,
-					'theme': 'monokai',
-					'autofocus': true,
-					'extraKeys': {
-						'Ctrl-Enter': function ( cm ) {
-							jQuery( 'form[action="/devel/php"]' ).submit( );
-						}
-					}
-				} );
-			}
+			'fn': loadCodeMirror
 		}
-	] );
-})( );
+	]);
+})();
