11fda87ccb75f0cfce2a09dcbbf0aba3ec3240d5
John <j.sanderson@ltl.uk.com>
2014-06-05 14:28:56 +0100

    updated to allow more fields

--- drupal-devel-php-improver/drupal-devel-php-improver.user.js
+++ drupal-devel-php-improver/drupal-devel-php-improver.user.js
@@ -4,8 +4,9 @@
 // @description   Adds CodeMirror to certain fields on a Drupal site
 // @namespace     http://www.9point6.com/userscript/drupal-php-input-improver
 // @include       */devel/php
-// @include       */admin/structure/ds/fields/manage_custom/*"
-// @version       1.1.0
+// @include       */admin/*"
+// @include       */node/*"
+// @version       1.2.0
 // ==/UserScript==
 
 (function ( ) {
@@ -42,80 +43,99 @@
 		document.head.appendChild( e );
 	};
 
+	// Helper method to load codeMirror
 	loadCodeMirror = function () {
 		var elem, selector, selection, page;
+
+		// Helper method to create a code mirror instance
+		// For some weird scoping reason it has to be in here.
+		createCodeMirror = function (elem, size) {
+			// Customise styles
+			e = document.createElement('style');
+			e.appendChild( document.createTextNode(''));
+			document.head.appendChild(e);
+			e.sheet.addRule( '.CodeMirror', [
+				'height: ' + size + 'px;',
+				'font-size: 1.4em;',
+				'font-family: "Inconsolata", "Source Code Pro", "Consolas", monospace;',
+			].join(' '));
+			e.sheet.addRule('.CodeMirror-gutters', [
+				'background: #36372F !important;'
+			].join(' '));
+
+			// Create CodeMirror on the textarea
+			window.codemirror = CodeMirror.fromTextArea(elem, {
+				'lineNumbers': true,
+				'mode': 'application/x-httpd-php-open',
+				'matchBrackets': true,
+				'autoCloseBrackets': true,
+				'indentUnit': 4,
+				'indentWithTabs': true,
+				'theme': 'monokai',
+				'autofocus': true,
+				'extraKeys': {
+					'Ctrl-Enter': function (cm) {
+						switch (page) {
+							case 'devel':
+								jQuery('form[action="/devel/php"]').submit();
+								break;
+
+							case 'field-code':
+								jQuery('form#ds-edit-custom-field-form').submit();
+								break;
+
+						}
+					}
+				}
+			});
+		};
 
 		// Determine element
 		if (document.location.pathname.indexOf('/devel/php') !== -1) {
 			page = 'devel';
 			elem = document.getElementById('edit-code');
+			createCodeMirror(elem, 600);
 
-		} else if (document.location.pathname.indexOf('/admin/structure/ds/fields/manage_custom') !== -1) {
-			page = 'display-suite';
-			selector = jQuery('.text-format-wrapper #edit-code-format--2');
+		} else if (document.location.pathname.indexOf('/admin/') !== -1 || document.location.pathname.indexOf('/node/add') !== -1) {
+			page = 'field-code';
+			selector = jQuery('.text-format-wrapper select, .form-textarea-wrapper select');
+
+			if(!selector) {
+				return;
+			}
+
 			selection = selector.val();
 
+			// Only load on certain field types
 			if(!window.initial) {
 				selector.click(function (e) {
-					selector = jQuery('.text-format-wrapper #edit-code-format--2');
+					selector = jQuery('.text-format-wrapper select, .form-textarea-wrapper select');
 					selection = selector.val();
-					if (selection === 'php' || selection === 'ds_code') {
-						jQuery('.CodeMirror').show();
+
+					if ((selection === 'php' || selection === 'ds_code') && window.codemirror === null) {
+						createCodeMirror(jQuery('.text-format-wrapper textarea:not(.text-summary)')[0], 300);
 					} else {
-						jQuery('.CodeMirror').hide();
+						try {
+							window.codemirror.toTextArea();
+						} catch (ex) {}
+						window.codemirror = null;
 					}
 				});
 				window.initial = true;
 			}
 
 			if (selection === 'php' || selection === 'ds_code') {
-				elem = jQuery('.text-format-wrapper textarea')[0];
+				elem = jQuery('.text-format-wrapper textarea:not(.text-summary)')[0];
 			} else {
 				jQuery('.CodeMirror').remove();
 				return;
 			}
+			createCodeMirror(elem, 300);
+
 		} else {
 			return;
 		}
 
-		// Customise styles
-		e = document.createElement('style');
-		e.appendChild( document.createTextNode(''));
-		document.head.appendChild(e);
-		e.sheet.addRule( '.CodeMirror', [
-			'height: 600px;',
-			'font-size: 1.4em;',
-			'font-family: "Inconsolata", "Source Code Pro", "Consolas", monospace;',
-		].join(' '));
-		e.sheet.addRule('.CodeMirror-gutters', [
-			'background: #36372F !important;'
-		].join(' '));
-
-		// Create CodeMirror on the textarea
-		CodeMirror.fromTextArea(elem, {
-			'lineNumbers': true,
-			'mode': 'application/x-httpd-php-open',
-			'matchBrackets': true,
-			'autoCloseBrackets': true,
-			'indentUnit': 4,
-			'indentWithTabs': true,
-			'theme': 'monokai',
-			'autofocus': true,
-			'extraKeys': {
-				'Ctrl-Enter': function (cm) {
-					switch (page) {
-						case 'devel':
-							jQuery('form[action="/devel/php"]').submit();
-							break;
-
-						case 'display-suite':
-							jQuery('form#ds-edit-custom-field-form').submit();
-							break;
-
-					}
-				}
-			}
-		});
 	};
 
 	// Let's get the party started
